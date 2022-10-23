f642583bb01217790212e470d116c204fbb5d51b
atoya200 <agustintoya2004@gmail.com>
2022-10-23 08:07:45 -0300

    Filtro para publicar en Twitter

---
+++ PII_Pipes_Filters_Start/src/Library/Filters/FilterPublished.cs
@@ -0,0 +1,27 @@
+﻿using System.Drawing;
+using System;
+using TwitterUCU;
+
+
+namespace CompAndDel.Filters
+{
+    /// <summary>
+    /// Un filtro que recibe una imagen y la retorna en escala de grises.
+    /// </remarks>
+    public class FilterPublished : IFilter
+    {
+        /// <summary>
+        /// Un filtro que retorna la imagen recibida con un filtro de escala de grises aplicado.
+        /// </summary>
+        /// <param name="image">La imagen a la cual se le va a aplicar el filtro.</param>
+        /// <returns>La imagen recibida pero en escala de grises.</returns>
+        public IPicture Filter(IPicture image)
+        {
+            IPicture result = image;
+            var twitter = new TwitterImage();
+            Console.WriteLine(twitter.PublishToTwitter(image.Name, @$"../..//Imgs/{image.Name}.jpg"));
+
+            return result;
+        }
+    }
+}
